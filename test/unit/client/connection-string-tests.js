dd1ef81f98b163bb8a9aeec4e197766c744aa0b3
Mike Nicholaides <mike.nicholaides@gmail.com>
2013-06-18 15:31:29 -0400

    Add semicolons to satisfy JSHint

--- test/unit/client/connection-string-tests.js
+++ test/unit/client/connection-string-tests.js
@@ -4,18 +4,17 @@ test("using connection string in client constructor", function() {
   var client = new Client("postgres://brian:pw@boom:381/lala");
   test("parses user", function() {
     assert.equal(client.user,'brian');
-  })
+  });
   test("parses password", function() {
     assert.equal(client.password, 'pw');
-  })
+  });
   test("parses host", function() {
     assert.equal(client.host, 'boom');
-  })
+  });
   test('parses port', function() {
-    assert.equal(client.port, 381)
-  })
+    assert.equal(client.port, 381);
+  });
   test('parses database', function() {
-    assert.equal(client.database, 'lala')
-  })
-})
-
+    assert.equal(client.database, 'lala');
+  });
+});
