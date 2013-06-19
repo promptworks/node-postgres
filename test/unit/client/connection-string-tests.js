629753d273a3ac8da4ff7314f7b3c0c8337ff7e7
Mike Nicholaides <mike.nicholaides@gmail.com>
2013-06-19 11:44:15 -0400

    Client constructor can take 2 config arguments, with the second overriding the first
    A connection string doesn't provide the ability to specify all the connection configuration, so this allows one to use a connection string and specify other configuration options like "ssl" and "binary".

--- test/unit/client/connection-string-tests.js
+++ test/unit/client/connection-string-tests.js
@@ -18,3 +18,34 @@ test("using connection string in client constructor", function() {
     assert.equal(client.database, 'lala');
   });
 });
+
+test("using connection string and a config object in client constructor", function() {
+  var client = new Client("postgres://brian:pw@boom:381/lala", {database: 'other_database', ssl: true});
+
+  test("retains parameters that aren't overridden", function() {
+    test("parses user", function() {
+      assert.equal(client.user,'brian');
+    });
+    test("parses password", function() {
+      assert.equal(client.password, 'pw');
+    });
+    test("parses host", function() {
+      assert.equal(client.host, 'boom');
+    });
+    test('parses port', function() {
+      assert.equal(client.port, 381);
+    });
+  });
+
+  test("overrides connection string parameters with ones from the config object", function() {
+    test('parses database', function() {
+      assert.equal(client.database, 'other_database');
+    });
+    test('parses database', function() {
+      assert.equal(client.ssl, true);
+    });
+  });
+
+});
+
+
